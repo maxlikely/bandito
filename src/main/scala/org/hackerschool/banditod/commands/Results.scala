69903f3bf8077e22587e88a69f0517c86de43b07
stungtoat <isaac@overttone.com>
2014-09-24 01:00:34 -0700

    remove notes

--- src/main/scala/org/hackerschool/banditod/commands/Results.scala
+++ src/main/scala/org/hackerschool/banditod/commands/Results.scala
@@ -4,32 +4,5 @@ package org.hackerschool.banditod.commands
 abstract class BanditoResults
 
 /** TODO: handle JSON serialization */
+
 case class IncrementResults(errors: List[String]) extends BanditoResults
-
-
-// abstract class Letter
-
-// case class A() extends Letter {
-//   val msg = "I'm a A"
-// }
-
-// case class B() extends Letter {
-//   val msg = "I'm a B"
-// }
-
-// case class C() extends Letter {
-//   val msg = "I'm a C"
-//   def get[T >: Letter](cls: T): T = {
-//     cls match {
-//       case x: B => {
-//         new B
-//       }
-//       case x: C => {
-//         new C
-//       }
-//       case x: A => {
-//         new A
-//       }
-//     }
-//   }
-// }
